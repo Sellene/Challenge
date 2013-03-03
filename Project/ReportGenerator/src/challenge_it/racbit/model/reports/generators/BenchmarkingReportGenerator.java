18c8d4b51370d16c7c9855a6c4ffe44c3a235a52
Cátia Moreira <catiaisabelmartinsmoreira@hotmail.com>
2013-03-03 15:39:10 +0000

    Merge complete

--- Project/ReportGenerator/src/challenge_it/racbit/model/reports/generators/BenchmarkingReportGenerator.java
+++ Project/ReportGenerator/src/challenge_it/racbit/model/reports/generators/BenchmarkingReportGenerator.java
@@ -24,6 +24,8 @@ import challenge_it.racbit.model.core.Product;
 import challenge_it.racbit.model.core.Product.SupplierType;
 import challenge_it.racbit.model.core.exceptions.CurrencyConversionException;
 import challenge_it.racbit.model.core.exceptions.ReportGenerationException;
+import challenge_it.racbit.model.reports.configurations.BenchmarkingReportConfiguration;
+import challenge_it.racbit.model.reports.configurations.BenchmarkingReportConfigurationReader;
 import challenge_it.racbit.model.reports.generators.utils.BenchmarkingReportInfo;
 import challenge_it.racbit.model.reports.generators.utils.BenchmarkingReportInfo.BenchmarkingDay;
 import challenge_it.racbit.model.reports.generators.utils.BenchmarkingReportInfo.BenchmarkingGroup;
@@ -33,33 +35,43 @@ import challenge_it.racbit.model.reports.generators.utils.CrossReference;
 
 
 public class BenchmarkingReportGenerator implements IReportGenerator {
-	private final CrossReference FIRST_CELL = new CrossReference(6, 5);
-	private final CrossReference LOCATION = new CrossReference(6, 1);
-	private final CrossReference GROUPS = new CrossReference(6, 2);
-	private final CrossReference DAYS = new CrossReference(6, 3);
-
+	/**
+	 * Path to the xml configuration file
+	 */
+	private static final String XML_CONFIGURATION = "configuration/Benchmarking_Reports/BenchmarkingReportConfiguration.xml";
+	
+	/**
+	 * Path to the xml schema file
+	 */
+	private static final String XML_SCHEMA = "configuration/Benchmarking_Reports/BenchmarkingReportSchema.xsd";
+	
+	/**
+	 * Path to the xml transformation file
+	 */
+	private static final String XML_TRANSFORMATION = "configuration/ReportTransformation.xsl";
+	
 	@Override
 	public void generate(Calendar reportDate, Country country, Iterable<Product> results) throws ReportGenerationException,
-	CurrencyConversionException {
-
-		HashMap<String, CrossReference> brokers = new HashMap<String, CrossReference>();
-
-		//BenchmarkingReportConfiguration config = (BenchmarkingReportConfiguration) new BenchmarkingReportConfigurationReader().read(XML_CONFIGURATION, XML_SCHEMA, XML_TRANSFORMATION);
-
-		BenchmarkingReportInfo info = processInformation(results, brokers);
-
+			CurrencyConversionException {
+		
+HashMap<String, CrossReference> brokers = new HashMap<String, CrossReference>();
+		
+		BenchmarkingReportConfiguration config = (BenchmarkingReportConfiguration) new BenchmarkingReportConfigurationReader().read(XML_CONFIGURATION, XML_SCHEMA, XML_TRANSFORMATION);
+		
+		BenchmarkingReportInfo info = processInformation(config, results, brokers);
+		
 		Workbook workbook = new XSSFWorkbook();
 		Sheet sheet = workbook.createSheet();
 		int offset = -1;
-
-		offset = fill(workbook, sheet, brokers, info.getRegulars(), HSSFColor.LIME.index, offset);
-
-		fill(workbook, sheet, brokers, info.getLowCosts(), HSSFColor.PINK.index, offset+1);
-
+		
+		offset = fill(workbook, sheet, config, brokers, info.getRegulars(), HSSFColor.LIME.index, offset);
+		
+		fill(workbook, sheet, config, brokers, info.getLowCosts(), HSSFColor.PINK.index, offset+1);
+		
 		FileOutputStream out;
 		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy HH_mm");
-
-		// TODO: Specify output file location 
+		
+		// TODO: Specify output file config.getLocationCell() 
 		// Do we net to put the country in the name?
 		try {
 			out = new FileOutputStream("Teste" + Calendar.getInstance().get(Calendar.MINUTE) + ".xlsx");
@@ -73,79 +85,79 @@ public class BenchmarkingReportGenerator implements IReportGenerator {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
-
+	
+		
+		
 	}
 
-	private int fill(Workbook workbook, Sheet sheet, HashMap<String, CrossReference> brokers, Map<String, BenchmarkingLocation> locations, short color, int offset) {
-
+	private int fill(Workbook workbook, Sheet sheet, BenchmarkingReportConfiguration config, HashMap<String, CrossReference> brokers, Map<String, BenchmarkingLocation> locations, short color, int offset) {
+		
 		Row row = null;
 		int groupOffset = offset+1;
-
+		
 		for (BenchmarkingLocation location : locations.values()) {
 			for(BenchmarkingGroup group : location.getGroups().values()){
 				for(BenchmarkingDay day : group.getDays().values()){
 					offset++;
-
-					row = sheet.getRow(FIRST_CELL.getRow() + offset);
-
+					
+					row = sheet.getRow(config.getGridValuesFirstCell().getRow() + offset);
+					
 					if(row == null)
-						row = sheet.createRow(FIRST_CELL.getRow() + offset);
-
-					Cell dayCell = row.createCell(DAYS.getColumn());
+						row = sheet.createRow(config.getGridValuesFirstCell().getRow() + offset);
+					
+					Cell dayCell = row.createCell(config.getLocationCell().getColumn());
 					dayCell.setCellValue(day.getNumberOfDays());
 					dayCell.setCellStyle(CellStyles.setThinBorders(CellStyles.setBold(CellStyles.getDefaultCellStyle(workbook), CellStyles.SMALL_TEXT, workbook)));
-
-					Cell groupCell = row.createCell(GROUPS.getColumn());
+					
+					Cell groupCell = row.createCell(config.getGroupCell().getColumn());
 					groupCell.setCellValue(group.getGroupName());
 					groupCell.setCellStyle(CellStyles.setThinBorders(CellStyles.setBold(CellStyles.getDefaultCellStyle(workbook), CellStyles.SMALL_TEXT, workbook)));
-
-					Cell locationCell = row.createCell(LOCATION.getColumn());
+					
+					Cell locationCell = row.createCell(config.getLocationCell().getColumn());
 					locationCell.setCellValue(location.getLocationName());
 					locationCell.setCellStyle(CellStyles.setBackground(CellStyles.setColor(CellStyles.getDefaultCellStyle(workbook), HSSFColor.WHITE.index, CellStyles.MEDIUM_TEXT, workbook), color));
-
+					
 					for (Product product : day.getProducts().values()) {
 						CrossReference crossReference = brokers.get(product.getBroker());
-
+												
 						Cell productPrice = row.createCell(crossReference.getColumn());
 						productPrice.setCellValue(product.getPrice());
 						productPrice.setCellType(Cell.CELL_TYPE_NUMERIC);
-
+						
 						Cell productSupplier = row.createCell(crossReference.getColumn()+1);
 						productSupplier.setCellValue(product.getSupplier());
-
+						
 						productPrice.setCellStyle(CellStyles.setBackground(CellStyles.setThinBorders(CellStyles.getDefaultCellStyle(workbook)), HSSFColor.LIGHT_GREEN.index));
 						productSupplier.setCellStyle(CellStyles.setBackground(CellStyles.setThinBorders(CellStyles.getDefaultCellStyle(workbook)), HSSFColor.LIGHT_GREEN.index));
-
+						
 					}
-				}    
+				}				
 			}
-			offset++; 
+			offset++;	
 			//row.createCell(offset);
-
+			
 			sheet.addMergedRegion(new CellRangeAddress(
-					LOCATION.getRow() + groupOffset, //first row (0-based)
-					LOCATION.getRow() + (offset-groupOffset-1) + groupOffset, //last row  (0-based)
-					LOCATION.getColumn(), //first column (0-based)
-					LOCATION.getColumn()  //last column  (0-based)
-					));
+					config.getLocationCell().getRow() + groupOffset, //first row (0-based)
+					config.getLocationCell().getRow() + (offset-groupOffset-1) + groupOffset, //last row  (0-based)
+					config.getLocationCell().getColumn(), //first column (0-based)
+					config.getLocationCell().getColumn()  //last column  (0-based)
+				));
 
 			groupOffset = offset+1; //update group offset
 		}
 		return offset;
 	}
 
-	private BenchmarkingReportInfo processInformation(Iterable<Product> results, HashMap<String, CrossReference> brokers) {
+	private BenchmarkingReportInfo processInformation(BenchmarkingReportConfiguration config, Iterable<Product> results, HashMap<String, CrossReference> brokers) {
 		BenchmarkingReportInfo info = new BenchmarkingReportInfo();
-		int column = 5;
-
+		int column = config.getGridValuesFirstCell().getColumn();
+		
 		for (Product product : results) {
 			BenchmarkingLocation location;
-
+			
 			if(product.getSupplierType() == SupplierType.LOW_COST){
 				location = info.checkLowCostLocation(product.getLocation());
-
+				
 				if(location == null){
 					location = info.new BenchmarkingLocation(product.getLocation());
 					info.addLowCost(product.getLocation(), location);
@@ -153,25 +165,24 @@ public class BenchmarkingReportGenerator implements IReportGenerator {
 			}
 			else{
 				location = info.checkRegularLocation(product.getLocation());
-
+				
 				if(location == null){
 					location = info.new BenchmarkingLocation(product.getLocation());
 					info.addRegular(product.getLocation(), location);
 				}
 			}
-
+			
 			saveProduct(info, location, product);
-
+		
 			if(brokers.get(product.getBroker()) == null){
 				brokers.put(product.getBroker(), new CrossReference(6, column));
 				column += 2;
 			}
-
+			
 		}
-
+		
 		return info;
 	}
-
 
 	private void saveProduct(BenchmarkingReportInfo info, BenchmarkingLocation location, Product product) {
 		BenchmarkingGroup group = location.checkGroup(product.getGroup());
@@ -179,13 +190,13 @@ public class BenchmarkingReportGenerator implements IReportGenerator {
 			group = info.new BenchmarkingGroup(product.getGroup());
 			location.addGroup(product.getGroup(), group);
 		}
-
+		
 		BenchmarkingDay day = group.checkDay(product.getNumberOfDays());
 		if(day == null){
 			day = info.new BenchmarkingDay(product.getNumberOfDays());
 			group.addDay(product.getNumberOfDays(), day);
 		}
-
+		
 		day.addProduct(product);
 	}
 }
